ea3cb7a1e86500de9d1a4e3dcaeee7f3e5fe8b10
Michael <105454702+Michael8P@users.noreply.github.com>
2023-12-04 19:49:51 -0500

    Add files via upload

---
+++ Convert_edgelist_to_matrix.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  2 20:24:34 2023
+
+@author: micha
+"""
+import networkx as nx
+import pandas as pd
+import csv
+# Read the edgelist from a CSV file where each row represents an interaction between two proteins
+edgelist = pd.read_csv("314315.csv")
+# Determine the list of unique proteins in the edgelist
+print(edgelist)
+
+# Reads edgelist from csv
+edge_tuples = []
+with open('314315.csv', 'r') as file:
+    csv_reader = csv.reader(file)
+    # Iterate through each row in the CSV file
+    for row in csv_reader:
+        # Assuming each row contains two nodes representing an edge
+        edge = tuple(row)  # Convert the row values to integers and create a tuple
+        edge_tuples.append(edge)  # Append the tuple to the list
+
+# Display the list of tuples
+print(edge_tuples)
+# create a graph from edgelist
+G = nx.from_edgelist(edge_tuples)
+print(G)
+
+# converts graph into an undirecteed adjacency matrix
+adj_matrix = nx.to_numpy_matrix(G, nodelist=G.nodes())
+print(adj_matrix)
