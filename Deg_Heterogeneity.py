8f6e4492c3cb204f31fa75018fce8cf9e70106de
Michael <105454702+Michael8P@users.noreply.github.com>
2024-01-18 20:50:12 -0500

    Create Deg_Heterogeneity.py

---
+++ Deg_Heterogeneity.py
@@ -0,0 +1,10 @@
+import scipy
+import numpy as np
+import pandas as pd
+def Deg_Heterogeneity(G):
+    if G.dtype != np.int32:
+        G = G.astype(np.int32)
+    ks = np.sum(G, axis=0)
+    N = np.shape(G[0])[0]    
+    deg_het_temp = np.sum(np.abs(ks - ks[:, np.newaxis])) / (N * (N - 1) * np.mean(ks))
+    return deg_het_temp
