d5dd0abfc18562f600b4abcbfab58a2612513580
Michael <105454702+Michael8P@users.noreply.github.com>
2024-01-18 20:50:56 -0500

    Update Rate_distortion_function.py

--- Rate_distortion_function.py
+++ Rate_distortion_function.py
@@ -51,36 +51,76 @@ def rate_distortion(G, heuristic, num_pairs):
 
     # Loop over the number of clusterings
     for i in range(N-1,1,-1):
-    # Different sets of node pairs to try:
+    #different sets of node pairs to try:
         if heuristic == 1:
-            # Try combining all pairs:
+        #try combining all pairs:
             pairs = np.array(list(itertools.combinations([x for x in range(i+1)], 2)))
             I = pairs[:,0]
             J = pairs[:,1]
         elif heuristic == 2:
-            # Pick num_pair node pairs at random
+            #pick num_pair node pairs at random
             pairs = np.array(list(itertools.combinations([x for x in range(i+1)], 2)))
-            inds = sample(size(pairs,1), np.min([num_pairs, size(pairs,1)])); 
-            I = pairs(inds,1)
-            J = pairs(inds,2)
+            inds = sample(range(size(pairs,1)), np.min([num_pairs, size(pairs,1)])); 
+            I = pairs[inds,0]
+            J = pairs[inds,1]
+        elif heuristic == 3:
+            # Try combining all pairs connected by an edge
+            I, J = np.where(np.triu(P_old + np.transpose(P_old), k=1))
+        elif heuristic == 4:
+            # pick num_paid nodes at random that are connected by an edge
+            I, J = np.where(np.triu(P_old + np.transpose(P_old), k=1))
+            pair_inds = sample(range(len(I)), min([num_pairs, len(I)]))
+            I = I[pair_inds]
+            J = J[pair_inds]
+        elif heuristic == 5:
+            # Pick num_pairs node pairs with largest joint transition probabilities
+            P_joint_symm = np.triu(P_joint + np.transpose(P_joint), k=1)
+            matrix = P_joint_symm.ravel()
+            K = min([num_pairs, np.sum(P_joint_symm.ravel() > 0)])
+            indices = np.argpartition(matrix, -K)[-K:]
+            indices = indices[np.argsort(matrix[indices])][::-1]
+            subscripts = np.unravel_index(indices, (i+1, i+1))
+            I = subscripts[0]
+            J = subscripts[1]
+        elif heuristic == 6:
+            # pick num_pair node pairs with largest joint transition probabilities plus self-transition probabilities
+            P_joint_symm = np.triu(P_joint + np.transpose(P_joint) + np.transpose(np.tile(np.diag(P_joint), (i+1,1))) + np.transpose(np.tile(np.transpose(np.diag(P_joint)), (i+1,1))) ,k=1)
+            matrix = P_joint_symm.ravel()
+            K = min([num_pairs, np.sum(P_joint_symm.ravel() > 0)])
+            indices = np.argpartition(matrix, -K)[-K:]
+            indices = indices[np.argsort(matrix[indices])][::-1]
+            subscripts = np.unravel_index(indices, (i+1, i+1))
+            I = subscripts[0]
+            J = subscripts[1]     
+        elif heuristic == 7:
+            # Pick num_pairs node pairs with largest combined stationary probabilities
+            P_ss_temp = np.triu(np.transpose(np.tile(p_ss_old, (i+1,1))) + np.tile(np.transpose(p_ss_old), (i+1,1)), k=1) 
+            matrix = P_ss_temp.ravel()
+            K = min([num_pairs,len(list(itertools.combinations(range(i+1), 2)))])
+            indices = np.argpartition(matrix, -K)[-K:]
+            indices = indices[np.argsort(matrix[indices])][::-1]
+            subscripts = np.unravel_index(indices, (i+1, i+1))
+            I = subscripts[0]
+            J = subscripts[1]         
         else:
             print('Variable "setting" is not properly defined.')
     
-        # Number of pairs 
+        #number of pairs 
         num_pairs_temp = len(I)
     
-        # Keep track of all entropies
+        #keep track of all entropies
         S_all = np.zeros((1, num_pairs_temp), dtype=float)
     
-        # Loop over the pairs of nodes:
+        #loop over the pairs of nodes:
         for ind in range(num_pairs_temp):
             ii = I[ind]
             jj = J[ind]
             inds_not_ij = list(range(0,(ii))) + list(range((ii+1),(jj))) + list(range((jj+1),(i+1)))
-    
-            # Compute new stationary distribution:           
+            # inds_not_ij = [x - 1 for x in inds_not_ij] issue if 0 then it becomes -1
+            #compute new stationary distribution:           
             p_ss_temp = np.append(p_ss_old[inds_not_ij], p_ss_old[ii] + p_ss_old[jj])
             
+            #stopped here
             # Compute new transition probabilities:
             P_temp_1 = np.sum(np.multiply(np.transpose(np.tile(p_ss_old[inds_not_ij], (2,1))), P_old[:, [ii,jj]][inds_not_ij]), axis = 1)
             P_temp_1 = P_temp_1 / p_ss_temp[0:-1]
@@ -97,30 +137,30 @@ def rate_distortion(G, heuristic, num_pairs):
             logP_temp_3 = np.array([logP_temp_3])
             logP_temp_3[logP_temp_3 == -inf] = 0
     
-            # Compute change in upper bound on mutual information
-            d1 = - sum(np.multiply(np.multiply(p_ss_temp[0:-1], P_temp_1), logP_temp_1))
+            #Compute change in upper bound on mutual information
+            d1 = -sum(np.multiply(np.multiply(p_ss_temp[0:-1], P_temp_1), logP_temp_1))
             d2 = - p_ss_temp[-1]*np.sum(np.multiply(P_temp_2,logP_temp_2))
-            d3 = p_ss_temp[-1]*P_temp_3*float(logP_temp_3[0])
+            d3 = - p_ss_temp[-1]*P_temp_3*logP_temp_3
+            d3 = d3[0]
             d4 = np.sum(np.multiply(p_ss_old, np.multiply(P_old[:,ii], logP_old[:, ii])))
             d5 = np.sum(np.multiply(np.multiply(p_ss_old, P_old[:,jj]), logP_old[:,jj]))
             d6 = p_ss_old[ii]*np.sum(np.multiply(P_old[ii,:],logP_old[ii,:]))
             d7 = p_ss_old[jj]*np.sum(np.multiply(P_old[jj,:], logP_old[jj,:]))
             d8 = - p_ss_old[ii]*(P_old[ii,ii]*logP_old[ii,ii] + P_old[ii,jj]*logP_old[ii,jj])
             d9 = - p_ss_old[jj]*(P_old[jj,jj]*logP_old[jj,jj] + P_old[jj,ii]*logP_old[jj,ii])
-            dS = d1+d2-d3+d4+d5+d6+d7+d8+d9
+            dS = d1+d2+d3+d4+d5+d6+d7+d8+d9
             S_temp = (S_old + dS)
             
         
-            # Keep track of all entropies:
+               # Keep track of all entropies:
             S_all[:,ind] = S_temp
     
     
     
-        # Find minimum entropy:
+         # Find minimum entropy:
         [dummy, min_inds] = np.nonzero(S_all == np.min(S_all))
         temp_mininds = list(min_inds)
-        rsample_mininds = random.sample(sorted(temp_mininds), k = 1)
-        min_ind = rsample_mininds[-1] 
+        min_ind = np.random.choice(sorted(temp_mininds))
     
         # Save mutual information:
         S_old = S_all[:,min_ind]
@@ -134,25 +174,32 @@ def rate_distortion(G, heuristic, num_pairs):
     
         p_ss_new = np.append(p_ss_old[inds_not_ij], p_ss_old[ii_new] + p_ss_old[jj_new])
         
-        P_joint = np.multiply(np.transpose(np.tile(p_ss_old, (i+1, 1))), P_old) 
+        
+        #P_joint changes
+        P_joint = np.multiply(np.transpose(np.tile(p_ss_old, (i+1, 1))), P_old) #this is good
         P_joint = np.c_[np.r_[P_joint[:,inds_not_ij][inds_not_ij,:],np.sum(P_joint[[ii_new,jj_new],:][:,inds_not_ij], axis=0, keepdims=True)], 
-                         np.append(np.sum((P_joint[:, [ii_new,jj_new]][inds_not_ij]), axis=1),np.sum(P_joint[np.ix_([ii_new,jj_new],[ii_new,jj_new])]))]        
+                         np.append(np.sum((P_joint[:, [ii_new,jj_new]][inds_not_ij]), axis=1),np.sum(P_joint[np.ix_([ii_new,jj_new],[ii_new,jj_new])]))]        #why did this change
+        
+        
+        
+        
         P_old = np.divide(P_joint, np.transpose(np.tile(p_ss_new, (i, 1))))
         p_ss_old = p_ss_new
-        
+    
         logP_old = np.log2(P_old)
         logP_old[logP_old == -inf] = 0
     
         # Record clusters and graph:
-        cluster1 = clusters[ii + 1][0:ii_new]
+        
+        cluster1 = clusters[i][0:ii_new]
         if (ii_new + 1) > (jj_new - 1):
             cluster2 = []
         else:
-            cluster2 = clusters[ii + 1][(ii_new + 1):(jj_new)]
+            cluster2 = clusters[i][(ii_new + 1):(jj_new)]
         if (jj_new + 1) >= (i + 1):
             cluster3 = []
         else:
-            cluster3 = clusters[ii + 1][(jj_new + 1): (i + 1)]
+            cluster3 = clusters[i][(jj_new + 1): (i + 1)]
             
         cluster4_ii = clusters[i][ii_new]
         cluster4_ii = [cluster4_ii] if isinstance(cluster4_ii,int) else cluster4_ii
